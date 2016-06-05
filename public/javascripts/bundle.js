d5f03ffdc2d6513fd055e88a3a633338195a0924
McMenemy <mr_mcmenemy@yahoo.com>
2016-06-05 14:11:43 -0700

    tweak hardcoded insights

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -24602,9 +24602,9 @@
 	      React.createElement(
 	        'div',
 	        { className: 'ui relaxed divided centered list' },
-	        React.createElement(InsightItem, { time: '22 minutes ago', message: 'Your data shows that running is highly correlated with increased happiness' }),
+	        React.createElement(InsightItem, { time: '22 minutes ago', message: 'Your data shows that days you run are highly correlated with increased happiness' }),
 	        React.createElement(InsightItem, { time: '12 hours ago', message: 'Your stress is higher than normal. In the past socializing with close friends/family has decreased your stress, while getting less sleep has increased your stress' }),
-	        React.createElement(InsightItem, { time: '1 day ago', message: 'Good job! You have exercised three days in the last week and our data shows that for many users similiar to yourself that reguluar exercise increses producivity and decreases stress' }),
+	        React.createElement(InsightItem, { time: '1 day ago', message: 'Good job! You have meditated/prayed three days in the last week and our data shows that for many users similiar to yourself that reguluar meditation/praying increases satisfaction and decreases stress' }),
 	        React.createElement(InsightItem, { time: '2 days ago', message: 'Looking for a book to read? Our data shows that many users similiar to you have had an improvement in satisfaction after reading The Alchemist by Paulo Coelho' }),
 	        React.createElement(InsightItem, { time: '3 days ago', message: 'You have answered your survey 21 days in row! Keep up the good work!' }),
 	        React.createElement(InsightItem, { time: '4 days ago', message: 'Your data shows that consuming caffeine has a slight negative effect on your productivity' }),
--- react/components/insightIndex.jsx
+++ react/components/insightIndex.jsx
@@ -7,11 +7,11 @@ var InsightIndex = React.createClass({
 
   render: function () {
     return (
-      <div>  
+      <div>
           <div className="ui relaxed divided centered list">
-            <InsightItem time="22 minutes ago" message="Your data shows that running is highly correlated with increased happiness" />
+            <InsightItem time="22 minutes ago" message="Your data shows that days you run are highly correlated with increased happiness" />
             <InsightItem time="12 hours ago" message="Your stress is higher than normal. In the past socializing with close friends/family has decreased your stress, while getting less sleep has increased your stress" />
-            <InsightItem time="1 day ago" message="Good job! You have exercised three days in the last week and our data shows that for many users similiar to yourself that reguluar exercise increses producivity and decreases stress" />
+            <InsightItem time="1 day ago" message="Good job! You have meditated/prayed three days in the last week and our data shows that for many users similiar to yourself that reguluar meditation/praying increases satisfaction and decreases stress" />
             <InsightItem time="2 days ago" message="Looking for a book to read? Our data shows that many users similiar to you have had an improvement in satisfaction after reading The Alchemist by Paulo Coelho" />
             <InsightItem time="3 days ago" message="You have answered your survey 21 days in row! Keep up the good work!" />
             <InsightItem time="4 days ago" message="Your data shows that consuming caffeine has a slight negative effect on your productivity" />
