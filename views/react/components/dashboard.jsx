45fdee0c27bf899a1e1e87b149b7dd54bef4a337
McMenemy <mr_mcmenemy@yahoo.com>
2016-06-12 16:52:28 -0700

    remove just for alan

--- views/react/components/dashboard.jsx
+++ views/react/components/dashboard.jsx
@@ -19,26 +19,6 @@ var Dashboard = React.createClass({
     this.setState({ shouldShowWatson: !(this.state.shouldShowWatson), clickCount: this.state.clickCount + 1 });
   },
 
-  justForAlan: function () {
-    if (this.state.clickCount > 1) {
-      return ('');
-    } else if (this.state.shouldShowWatson) {
-      return (
-        <div>
-          <div className="ui circular green button" onClick={this.clickRedButton}>Do Click</div>
-          <div className="ui large image">
-            <img src={'http://www.imgjunk.com/wp-content/uploads/2015/05/Emma-Watson-Looking-Glamorous.jpg'} />
-            <h1 className="ui header">told you the UI would get you hard ;)</h1>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="ui circular red button" onClick={this.clickRedButton}>Do Not Click</div>
-      );
-    }
-  },
-
   clickSurvey: function () {
     var url = AuthStore.currentUser().formURL;
     var win = window.open(url, '_blank');
@@ -51,7 +31,6 @@ var Dashboard = React.createClass({
       <div className="ui container">
         <Navbar />
         <div className="ui blue button" onClick={this.clickSurvey}>Fill out daily survey</div>
-        {this.justForAlan()}
         <h1 className="ui left aligned header">Your Data Streams</h1>
         <DataStreamIndex />
         <h1 className="ui header">Your Insights</h1>
