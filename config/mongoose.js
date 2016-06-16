383f4048fa858bcbbe9eac3f617f0b85632763d8
Alan Yan <acyanlight@gmail.com>
2016-06-16 15:10:40 -0400

    delete comments

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,8 +4,6 @@ var mongoose = require('mongoose');
 module.exports = function () {
   var db = mongoose.connect(config.db);
 
-  // load models
-
   mongoose.connection.once('connected', function (err) {
     if (err) throw err;
     console.log('connected to ' + process.env.NODE_ENV + ' database...');
