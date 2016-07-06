b8e3e68e2b8f30309ab5687ea7b7f732ca8260dd
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-06 11:54:40 -0700

    fitbit: stuck on response_type and cross-origin

--- config/passport/fitbit.js
+++ config/passport/fitbit.js
@@ -5,7 +5,8 @@ module.exports = function (passport) {
   passport.use(new FitbitStrategy({
       clientID:     '227TQM',
       clientSecret: 'd44c4411eec72933ccd86d7efe8ea12d',
-      callbackURL: 'http://exzume.com/auth/fitbit/callback',
+      callbackURL: '/auth/fitbit/callback/',
+      authorizationURL: 'https://www.fitbit.com/oauth2/authorize?response_type=code'
     },
     function (accessToken, refreshToken, profile, done) {
       console.log('hi josh');
