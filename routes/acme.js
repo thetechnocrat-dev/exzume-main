00daf074e3cfc7ba24f25ff2d9624f8e09f8ddd7
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-07 18:16:35 -0700

    acme challenge take 3

--- routes/acme.js
+++ routes/acme.js
@@ -3,4 +3,9 @@ module.exports = function (router) {
   router.get('/8OwHXXTcGWAgPa09nU1QRDmDtdNvQillQUXT3Rh85hg', function (req, res) {
     res.send('8OwHXXTcGWAgPa09nU1QRDmDtdNvQillQUXT3Rh85hg.0wCL6T35wH1HkXWY_2odZ64L0no2f27NrtpJDrMi4qU');
   });
+
+  router.get('/hrV-sNM6Ug23a1d3CPhKCn6761E6mAHZ_igVJWRojGg', function (req, res) {
+    res.send('hrV-sNM6Ug23a1d3CPhKCn6761E6mAHZ_igVJWRojGg.0wCL6T35wH1HkXWY_2odZ64L0no2f27NrtpJDrMi4qU');
+  });
+
 };
