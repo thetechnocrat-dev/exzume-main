977194a52b95889821b9ee92bb3d9539bdecd252
Alan Yan <acyanlight@gmail.com>
2016-07-09 11:42:13 -0700

    basic splash template done

--- views/react/components/splashTemp.jsx
+++ views/react/components/splashTemp.jsx
@@ -3,17 +3,20 @@ var React = require('react');
 var SplashTemp = React.createClass({
 
   render: function () {
+    var stepsContainerStyle = { marginTop: '5%', };
+    var stepStyle = { fontSize: '1.5em', };
+    var panelStyle = { color: '#black' };
+
     return (
       <div>
         <div className="ui large top fixed hidden menu">
           <div className="ui container">
             <a className="active item">Home</a>
-            <a className="item">Work</a>
-            <a className="item">Company</a>
-            <a className="item">Careers</a>
+            <a className="item">About</a>
+            <a className="item">Contact</a>
             <div className="right menu">
               <div className="item">
-                <a className="ui button">Log in</a>
+                <a className="ui button">Sign In</a>
               </div>
               <div className="item">
                 <a className="ui primary button">Sign Up</a>
@@ -24,10 +27,9 @@ var SplashTemp = React.createClass({
 
         <div className="ui vertical inverted sidebar menu">
           <a className="active item">Home</a>
-          <a className="item">Work</a>
-          <a className="item">Company</a>
-          <a className="item">Careers</a>
-          <a className="item">Login</a>
+          <a className="item">About</a>
+          <a className="item">Contact</a>
+          <a className="item">Signin</a>
           <a className="item">Signup</a>
         </div>
 
@@ -40,11 +42,10 @@ var SplashTemp = React.createClass({
                   <i className="sidebar icon"></i>
                 </a>
                 <a className="active item">Home</a>
-                <a className="item">Work</a>
-                <a className="item">Company</a>
-                <a className="item">Careers</a>
+                <a className="item">About</a>
+                <a className="item">Contact</a>
                 <div className="right item">
-                  <a className="ui inverted button">Log in</a>
+                  <a className="ui inverted button">Sign In</a>
                   <a className="ui inverted button">Sign Up</a>
                 </div>
               </div>
@@ -52,37 +53,57 @@ var SplashTemp = React.createClass({
 
             <div className="ui text container">
               <h1 className="ui inverted header">
-                Imagine-a-Company
+                exzume
               </h1>
-              <h2>Do whatever you want when you want to.</h2>
+              <h2>cultivate your data.</h2>
               <div className="ui huge primary button">Get Started <i className="right arrow icon"></i></div>
             </div>
 
           </div>
 
+          <div id="aboutAnchor">
+
+          </div>
+
+
           <div className="ui vertical stripe segment">
-            <div className="ui middle aligned stackable grid container">
+            <div className="ui center aligned stackable grid container">
               <div className="row">
-                <div className="eight wide column">
-                  <h3 className="ui header">We Help Companies and Companions</h3>
-                  <p>We can give your company superpowers to do things that they never thought possible. Let us delight your customers and empower your needs...through pure data analytics.</p>
-                  <h3 className="ui header">We Make Bananas That Can Dance</h3>
-                  <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.</p>
+                <div className="sixteen wide column">
+                  <h1 className="ui header">Exzume Creates Useful Insights from Your Data</h1>
+                  <h2 className="ui header">Here's How to Use It...</h2>
                 </div>
-                <div className="six wide right floated column">
-                  <img src="assets/images/wireframe/white-image.png" className="ui large bordered rounded image" />
-                  </div>
+                <div className="ui center aligned grid container" style={stepsContainerStyle}>
+                  <div className="three column stackable row">
+
+                    <div className="column">
+                      <div style={panelStyle}>
+                        <i className="massive refresh icon"></i>
+                        <h2>Sync devices & apps you already use (e.g. Fitbit, Spotify, RescueTime).</h2>
+                      </div>
+                    </div>
+
+                    <div className="column">
+                      <div style={panelStyle}>
+                        <i className="massive list layout icon"></i>
+                        <h2>Fill out a short daily survey dynamically customized for you.</h2>
+                      </div>
+                    </div>
+
+                    <div className="column">
+                      <div style={panelStyle}>
+                        <i className="massive checkmark icon"></i>
+                        <h2>That's it! We do all the data crunching & turn your data into useful insights.</h2>
+                      </div>
+                    </div>
+
                 </div>
-                <div className="row">
-                  <div className="center aligned column">
-                    <a className="ui huge button">Check Them Out</a>
-                  </div>
-                </div>
+              </div>
+              </div>
               </div>
             </div>
 
-
-            <div className="ui vertical stripe quote segment">
+            {/*<div className="ui vertical stripe quote segment">
               <div className="ui equal width stackable internally celled grid">
                 <div className="center aligned row">
                   <div className="column">
@@ -97,19 +118,20 @@ var SplashTemp = React.createClass({
                     </div>
                   </div>
                 </div>
-              </div>
+              </div>*/}
 
               <div className="ui vertical stripe segment">
                 <div className="ui text container">
-                  <h3 className="ui header">Breaking The Grid, Grabs Your Attention</h3>
-                  <p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
+                  <h3 className="ui header">Why We Made Exzume</h3>
+                  <p>There are many apps and wearable devices that collect data about our lives. However, this data is hardly ever used to provide actionable insights that we can actualize to improve the quality of our lives. </p>
                   <a className="ui large button">Read More</a>
-                  <h4 className="ui horizontal header divider">
-                    <a href="#">Case Studies</a>
+                  {/*<h4 className="ui horizontal header divider">
+                    <a href="#">Success Stories</a>
                   </h4>
-                  <h3 className="ui header">Did We Tell You About Our Bananas?</h3>
+                  <h3 className="ui header">Did We Tell You About Michael?</h3>
                   <p>Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.</p>
                   <a className="ui large button">I'm Still Quite Interested</a>
+                  */}
                 </div>
               </div>
 
@@ -120,24 +142,20 @@ var SplashTemp = React.createClass({
                     <div className="three wide column">
                       <h4 className="ui inverted header">About</h4>
                       <div className="ui inverted link list">
-                        <a href="#" className="item">Sitemap</a>
+                        <a href="#" className="item">Privacy Policy</a>
                         <a href="#" className="item">Contact Us</a>
-                        <a href="#" className="item">Religious Ceremonies</a>
-                        <a href="#" className="item">Gazebo Plans</a>
                       </div>
                     </div>
                     <div className="three wide column">
                       <h4 className="ui inverted header">Services</h4>
                       <div className="ui inverted link list">
-                        <a href="#" className="item">Banana Pre-Order</a>
-                        <a href="#" className="item">DNA FAQ</a>
-                        <a href="#" className="item">How To Access</a>
-                        <a href="#" className="item">Favorite X-Men</a>
+                        <a href="https://www.myvessyl.com/" className="item">Vessyl Pre-Order</a>
+                        <a href="#" className="item">Exzume FAQ</a>
                       </div>
                     </div>
                     <div className="seven wide column">
-                      <h4 className="ui inverted header">Footer Header</h4>
-                      <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                      <h4 className="ui inverted header">© exzume 2016</h4>
+                      <p>We could save your life.</p>
                     </div>
                   </div>
                 </div>
