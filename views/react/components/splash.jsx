f4542684f914e0bb9de05fdb0140a94f6477037b
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-09 17:38:19 -0700

    re add top nav

--- views/react/components/splash.jsx
+++ views/react/components/splash.jsx
@@ -106,7 +106,23 @@ var Splash = React.createClass({
   },
 
   makeTopNavBar: function () {
-
+    return (
+      <div className="ui container">
+        <div className="ui large secondary inverted pointing menu">
+          <a className="toc item">
+            <i className="sidebar icon"></i>
+          </a>
+          <a className="active item">Home</a>
+          <a className="item">Work</a>
+          <a className="item">Company</a>
+          <a className="item">Careers</a>
+          <div className="right item">
+            <a className="ui inverted button">Log in</a>
+            <a className="ui inverted button">Sign Up</a>
+          </div>
+        </div>
+      </div>
+    );
   },
 
   makeStickyNavRightButtons: function () {
@@ -180,6 +196,8 @@ var Splash = React.createClass({
         <SideNavbar />
 
         <div className="ui inverted vertical masthead center aligned segment" style={mastheadStyle}>
+
+          {this.makeTopNavBar()}
 
           <div className="ui text container">
             <h1 className="ui inverted header">
