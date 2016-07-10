5a34b3582a46b07e3893411c5084a3a402ffd7df
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-09 20:33:11 -0700

    add hamburger menu

--- views/react/components/sideNavbar.jsx
+++ views/react/components/sideNavbar.jsx
@@ -1,20 +1,23 @@
-  var React = require('react');
-  var PropTypes = React.PropTypes;
+var React = require('react');
 
-  var SideNavbar = React.createClass({
+var SideNavbar = React.createClass({
 
-    render: function() {
-      return (
-        <div className="ui vertical inverted sidebar menu">
-          <a className="active item">Home</a>
-          <a className="item">About</a>
-          <a className="item">Contact</a>
-          <a className="item">Signin</a>
-          <a className="item">Signup</a>
-        </div>
-      );
-    }
+  componentDidMount: function () {
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+  },
 
-  });
+  render: function () {
+    return (
+      <div className="ui vertical inverted sidebar menu">
+        <a className="active item">Home</a>
+        <a className="item">About</a>
+        <a className="item">Contact</a>
+        <a className="item">Signin</a>
+        <a className="item">Signup</a>
+      </div>
+    );
+  },
 
-  module.exports = SideNavbar;
+});
+
+module.exports = SideNavbar;
