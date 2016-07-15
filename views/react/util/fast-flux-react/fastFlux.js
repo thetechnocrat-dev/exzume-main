ea8548fbd43d59e2cb04b06f71236fea9237ac83
McMenemy <mr_mcmenemy@yahoo.com>
2016-07-15 12:02:06 -0700

    document fast flux

--- views/react/util/fast-flux-react/fastFlux.js
+++ views/react/util/fast-flux-react/fastFlux.js
@@ -4,13 +4,22 @@
 // then do FastFlux.webCycle(method, url, options)
 //    method: the HTTP method you want to use all lowercase ('put', 'post', or 'get')
 //    url: the back end route you want to hit
-//    options: a hash of different options which are defined below
-//        success: function    (front end successCallback)
-//        error: function      (front end errorCallback)
-//        shouldReceive
+//    options: an object of different options which are defined below
+//        body: object (data to be sent in AJAX, required for post and put requests)
+//        success: function    (optional front end successCallback)
+//        error: function      (optional front end errorCallback)
+//        shouldReceive: boolean  (if the backend data should end up in a store)
+//        type: the name of the store action that received data (required if shouldReceive is true)
 
-
-
+// example
+// var signUpBody = {
+//   username: this.state.username,
+//   password: this.state.password,
+//   email: this.state.email,
+// };
+// FastFlux.webCycle('post', '/signup', {
+//   body: signUpBody,
+// });
 
 var Dispatcher = require('../../dispatcher/dispatcher');
 
