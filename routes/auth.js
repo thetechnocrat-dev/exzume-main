3320d8c54ab9d6a99c8f346c1d8e1d17bd07802a
McMenemy <mr_mcmenemy@yahoo.com>
2016-08-17 10:25:58 -0700

    another console.log

--- routes/auth.js
+++ routes/auth.js
@@ -150,6 +150,7 @@ module.exports = function (router, passport) {
   );
 
   router.get('/datastreams/:datastream/callback', function (req, res) {
+      console.log(req.params);
       console.log(req.params.datastream + ' callback route');
       passport.authenticate(req.params.datastream, {
         successRedirect: '/#/dashboard?=', // redirect to grab from API and redirect
