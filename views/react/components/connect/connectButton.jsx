3e76ab6b8235791fdb0977927c6753e17f481038
Alan Yan <acyanlight@gmail.com>
2016-09-27 10:56:51 -0700

    darksky works for first connect with error and success msgs

--- views/react/components/connect/connectButton.jsx
+++ views/react/components/connect/connectButton.jsx
@@ -9,10 +9,16 @@ var ConnectButton = React.createClass({
     connectUrl: React.PropTypes.string.isRequired,
   },
 
+  getInitialState: function () {
+    return ({ loading: false, errorMsg: '', successMsg: '' });
+  },
+
   requestUserLocation: function (connectUrl) {
+    var _this = this;
 
     if (!navigator.geolocation) {
       console.log('Geolocation is not supported by your browser');
+      this.setState({ loading: false, errorMsg: 'Geolocation is not supported by your browser' });
       return;
     }
 
@@ -28,6 +34,10 @@ var ConnectButton = React.createClass({
       if (lat & long) {
         FastFlux.webCycle('post', connectUrl, {
           body: userLoc,
+          success: _this.successCallback,
+          error: _this.errorCallback,
+          shouldStoreReceive: true,
+          storeActionType: 'SESSION_RECEIVED',
         });
       };
     };
@@ -39,11 +49,21 @@ var ConnectButton = React.createClass({
     navigator.geolocation.getCurrentPosition(success, error);
   },
 
+  successCallback: function () {
+    this.setState({ loading: false, successMsg: 'Your location has been saved' });
+  },
+
+  errorCallback: function (err) {
+    this.setState({ loading: false, errorMsg: 'Unable to retrieve your location' });
+  },
+
   handleClick: function () {
     if (this.props.isConnected) {
       window.open(this.props.openUrl, '_blank');
     } else {
       if (this.props.appName == 'DarkSky') {
+        // will remain in loading state until AJAX callback changes state
+        this.setState({ loading: true });
         this.requestUserLocation(this.props.connectUrl);
       } else {
         window.open(this.props.connectUrl, '_self');
@@ -52,21 +72,55 @@ var ConnectButton = React.createClass({
   },
 
   makeButton: function () {
-    return (
-      <button
-        className="ui green labeled mini icon button"
-        onClick={this.handleClick}
-      >
-        <i className="plus icon" />
-        {this.props.isConnected ? 'open' : 'connect'}
-      </button>
-    );
+    if (this.state.loading) {
+      return (
+        <button
+          className="ui green disabled loading labeled mini icon button"
+        >
+          <i className="plus icon" />
+          {this.props.isConnected ? 'open' : 'connect'}
+        </button>
+      );
+    } else {
+      return (
+        <button
+          className="ui green labeled mini icon button"
+          onClick={this.handleClick}
+        >
+          <i className="plus icon" />
+          {this.props.isConnected ? 'open' : 'connect'}
+        </button>
+      );
+    }
+  },
+
+  makeMessage: function () {
+    if (this.state.errorMsg.length > 0) {
+      return (
+        <div className="ui error message">
+          <i className="close icon"></i>
+          <div className="header">
+            {this.state.errorMsg}
+          </div>
+        </div>
+      );
+    } else if (this.state.successMsg.length > 0) {
+      return (
+        <div className="ui success message">
+          <i className="close icon"></i>
+          <div className="header">
+            {this.state.successMsg}
+          </div>
+        </div>
+      );
+    }
   },
 
   render: function () {
     return (
       <div>
         {this.makeButton()}
+        {this.makeMessage()}
       </div>
     );
   },
