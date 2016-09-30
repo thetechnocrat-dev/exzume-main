57ea8275150d5f1a4cc343174d23e6f9be6b0123
McMenemy <mr_mcmenemy@yahoo.com>
2016-09-30 09:43:34 -0700

    fix compare dropdown green circle bug

---
+++ views/react/components/exploreV2/addFeatureDropdownFeatureItem.jsx
@@ -0,0 +1,50 @@
+var React = require('react');
+var FastFlux = require('../../util/fast-flux-react/fastFlux');
+var Style = require('../../util/style');
+
+var AddFeatureDropdownFeatureItem = React.createClass({
+  propTypes: {
+    feature: React.PropTypes.object.isRequired,
+  },
+
+  getInitialState: function () {
+    return { isSelected: false };
+  },
+
+  clickItem: function () {
+    if (this.state.isSelected) {
+      FastFlux.cycle('TIME_SERIES_REMOVED', this.props.feature);
+    } else {
+      FastFlux.cycle('TIME_SERIES_RECEIVED', this.props.feature);
+    }
+
+    this.setState({ isSelected: !this.state.isSelected });
+  },
+
+  render: function () {
+    if (this.state.isSelected) {
+      return (
+        <div
+          className="item"
+          onClick={this.clickItem}
+        >
+          <div className="ui green empty circular label"></div>
+          {this.props.feature.name}
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="item"
+          onClick={this.clickItem}
+        >
+          {this.props.feature.name}
+        </div>
+      );
+    }
+  },
+
+});
+
+module.exports = AddFeatureDropdownFeatureItem;
+
--- views/react/components/exploreV2/addFeatureDropdownItem.jsx
+++ views/react/components/exploreV2/addFeatureDropdownItem.jsx
@@ -1,51 +1,18 @@
 var React = require('react');
-var FastFlux = require('../../util/fast-flux-react/fastFlux');
-var Style = require('../../util/style');
+
+// Components
+var AddFeatureDropdownFeatureItem = require('./addFeatureDropdownFeatureItem');
 
 var DropdownItem = React.createClass({
   propTypes: {
     dataStream: React.PropTypes.object.isRequired,
   },
 
-  getInitialState: function () {
-    return { isSelected: false };
-  },
-
-  clickItem: function (feature) {
-    if (this.state.isSelected) {
-      FastFlux.cycle('TIME_SERIES_REMOVED', feature);
-    } else {
-      FastFlux.cycle('TIME_SERIES_RECEIVED', feature);
-    }
-
-    this.setState({ isSelected: !this.state.isSelected });
-  },
-
   makeFeatureItems: function () {
-    var _this = this;
     return this.props.dataStream.features.map(function (feature, idx) {
-      if (_this.state.isSelected) {
-        return (
-          <div
-            key={idx}
-            className="item"
-            onClick={_this.clickItem.bind(null, feature)}
-          >
-            <div className="ui green empty circular label"></div>
-            {feature.name}
-          </div>
-        );
-      } else {
-        return (
-          <div
-            key={idx}
-            className="item"
-            onClick={_this.clickItem.bind(null, feature)}
-          >
-            {feature.name}
-          </div>
-        );
-      }
+      return (
+        <AddFeatureDropdownFeatureItem key={idx} feature={feature} />
+      );
     });
   },
 
