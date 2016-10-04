d2b0a0ca450f415a3a020d4f3cae1e879581e7c4
McMenemy <mr_mcmenemy@yahoo.com>
2016-10-03 21:31:34 -0700

    reorganize dashboard columns

--- views/react/components/dashboard/dashboard.jsx
+++ views/react/components/dashboard/dashboard.jsx
@@ -53,42 +53,104 @@ var Dashboard = React.createClass({
 
     // the order you put cards here is the same order they will be going left to right
     cards.push(
-      <div className="column" key={'mood'}>
-        <MoodCard user={this.state.user} />
-      </div>
+      <MoodCard user={this.state.user} />
     );
 
     if (this.state.user.datastreams.fitbit.isConnected) {
       cards.push(
-        <div className="column" key={'fitbit'}>
-          <FitbitCard fitbit={this.state.user.datastreams.fitbit} />
-        </div>
+        <FitbitCard fitbit={this.state.user.datastreams.fitbit} />
       );
     }
 
     if (this.state.user.datastreams.rescuetime.isConnected) {
-      return (
-        <div className="column" key={'rescuetime'}>
-          <RescueTimeCard rescuetime={this.state.user.datastreams.rescuetime} />
-        </div>
+      cards.push(
+        <RescueTimeCard rescuetime={this.state.user.datastreams.rescuetime} />
       );
     }
 
     if (this.state.user.datastreams.darksky.isConnected) {
-      return (
-        <div className="column" key={'darksky'}>
-          <DarkSkyCard darksky={this.state.user.datastreams.darksky} />
-        </div>
+      cards.push(
+        <DarkSkyCard darksky={this.state.user.datastreams.darksky} />
       );
     }
 
     cards.push(
-      <div className="column" key={'connect'}>
-        <ConnectionCard connectedStreams={SessionStore.getUserStreams()} />
-      </div>
+      <ConnectionCard connectedStreams={SessionStore.getUserStreams()} />
     );
 
     return cards;
+  },
+
+  makeColumn: function (columnNum, columnCount, cards) {
+    // so that when i % columnCount equals zero it will go in last column
+    if (columnNum === columnCount) {
+      columnNum = 0;
+    }
+
+    console.log(columnNum);
+
+    var cardsFilterd = cards.filter(function (card, i) {
+      console.log((i + 1) % columnCount === columnNum);
+      return (i + 1) % columnCount === columnNum;
+    });
+
+    console.log(cards);
+    console.log(cardsFilterd);
+    return cardsFilterd;
+  },
+
+  makeColumns: function (columnCount, rowClassName, cards) {
+    if (columnCount === 1) {
+      return (
+        <div className={rowClassName}>
+          <div className="column">
+            {this.makeColumn(1, columnCount, cards)}
+          </div>
+        </div>
+      );
+    } else if (columnCount === 2) {
+      return (
+        <div className={rowClassName}>
+          <div className="column">
+            {this.makeColumn(1, columnCount, cards)}
+          </div>
+          <div className="column">
+            {this.makeColumn(2, columnCount, cards)}
+          </div>
+        </div>
+      );
+    } else if (columnCount === 3) {
+      return (
+        <div className={rowClassName}>
+          <div className="column">
+            {this.makeColumn(1, columnCount, cards)}
+          </div>
+          <div className="column">
+            {this.makeColumn(2, columnCount, cards)}
+          </div>
+          <div className="column">
+            {this.makeColumn(3, columnCount, cards)}
+          </div>
+        </div>
+      );
+    } else if (columnCount === 4) {
+      return (
+        <div className={rowClassName}>
+          <div className="column">
+            {this.makeColumn(1, columnCount, cards)}
+          </div>
+          <div className="column">
+            {this.makeColumn(2, columnCount, cards)}
+          </div>
+          <div className="column">
+            {this.makeColumn(3, columnCount, cards)}
+          </div>
+          <div className="column">
+            {this.makeColumn(4, columnCount, cards)}
+          </div>
+        </div>
+      );
+    }
   },
 
   makeContent: function () {
@@ -98,24 +160,29 @@ var Dashboard = React.createClass({
     var LARGE_MONITOR = 1200;
     var SMALL_MONITOR = 992;
     var TABLET = 768;
+    var rowClassName;
+    var columnCount;
 
     if (this.state.viewPortWidth > LARGE_MONITOR) {
       rowClassName = 'four column row';
+      columnCount = 4;
     } else if (this.state.viewPortWidth > SMALL_MONITOR) {
       rowClassName = 'three column row';
+      columnCount = 3;
     } else if (this.state.viewPortWidth > TABLET) {
       rowClassName = 'two column row';
+      columnCount = 2;
     } else {
       rowClassName = 'one column row';
+      columnCount = 1;
     }
 
     if (user) {
+      var cards = this.makeCards();
       return (
         <div className="ui container">
           <div className="ui grid">
-            <div className={rowClassName}>
-              {this.makeCards()}
-            </div>
+            {this.makeColumns(columnCount, rowClassName, cards)}
           </div>
         </div>
       );
